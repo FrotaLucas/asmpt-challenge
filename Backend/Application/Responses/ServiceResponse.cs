804e21ec10a27936456e0e5670cca0e192038449
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-01 20:09:22 +0100

    create ServiceResponse

---
+++ Backend/Application/Responses/ServiceResponse.cs
@@ -0,0 +1,9 @@
+namespace Backend.Application.Responses
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; } = String.Empty;
+    }
+}
