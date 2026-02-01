e061aafa29b4e465d7c65ba97984979f5a40cb8d
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-01 20:27:51 +0100

    implement Logging

--- Backend/Application/Services/BoardService.cs
+++ Backend/Application/Services/BoardService.cs
@@ -1,6 +1,7 @@
 using Backend.Infrastructure.Data;
 using Backend.Domain.Entities;
 using Backend.Domain.Interfaces;
+using Backend.Application.Responses;
 
 namespace Backend.Application.Services
 {
@@ -8,41 +9,61 @@ namespace Backend.Application.Services
     {
         private readonly DataContext _context;
 
-        //private readonly ILogger<BoardService> _logger;
+        private readonly ILogger<BoardService> _logger;
 
         //LogError
         //_logger.LogError(ex, "Error fetching boards");
 
-        public BoardService(DataContext context)
+        public BoardService(DataContext context, ILogger<BoardService> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-
-
-        public async Task<List<Board>> GetAllBoardsAsync()
+        public async Task<ServiceResponse<List<Board>>> GetAllBoardsAsync()
         {
             List<Board> boards = _context.Boards.ToList();
 
-            return boards;
+            if(boards == null || boards.Count == 0)
+            {
+                var response = new ServiceResponse<List<Board>>()
+                {
+                    Data = null,
+                    Success = false,
+                    Message = "No boards found."
+                };
+                
+                _logger.LogWarning("No boards found in the database.");
+                return response;
+            }
+
+            return new ServiceResponse<List<Board>>()
+            {
+                Data = boards,
+                Success = true
+            };
         }
 
-        public Task<Board> GetBoardByIdAsync(int id)
+        public Task<ServiceResponse<Board>> GetBoardByIdAsync(int id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<Board> CreateBoardAsync(Board board)
+        public async Task<ServiceResponse<Board>> CreateBoardAsync(Board board)
         {
             await _context.Boards.AddAsync(board);
             await _context.SaveChangesAsync();
-            return board;
+            return new ServiceResponse<Board>()
+            {
+                Data = board,
+                Success = true
+            };
         }
-        public Task<bool> DeleteBoardAsync(int id)
+        public Task<ServiceResponse<bool>> DeleteBoardAsync(int id)
         {
             throw new NotImplementedException();
         }
-        public Task<Board> UpdateBoardAsync(Board board)
+        public Task<ServiceResponse<Board>> UpdateBoardAsync(Board board)
         {
             throw new NotImplementedException();
         }
