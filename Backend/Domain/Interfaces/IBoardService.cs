6dc95c3d00dd297aa8470dc16e0d9030e9722285
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-01 20:27:43 +0100

    refactor methods

--- Backend/Domain/Interfaces/IBoardService.cs
+++ Backend/Domain/Interfaces/IBoardService.cs
@@ -1,18 +1,19 @@
+using Backend.Application.Responses;
 using Backend.Domain.Entities;
 
 namespace Backend.Domain.Interfaces
 {
     public interface IBoardService
     {
-        Task<List<Board>> GetAllBoardsAsync();
+        Task<ServiceResponse<List<Board>>> GetAllBoardsAsync();
 
-        Task<Board> GetBoardByIdAsync(int id);
+        Task<ServiceResponse<Board>> GetBoardByIdAsync(int id);
 
-        Task<Board> CreateBoardAsync(Board board);
+        Task<ServiceResponse<Board>> CreateBoardAsync(Board board);
 
-        Task<bool> DeleteBoardAsync(int id);
-
-        Task<Board> UpdateBoardAsync(Board board);
+        Task<ServiceResponse<bool>> DeleteBoardAsync(int id);   
+        
+        Task<ServiceResponse<Board>> UpdateBoardAsync(Board board);
 
     }
 
