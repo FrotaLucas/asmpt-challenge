50ce08e7db3d3db9bb7519a5ca6ebedbaf9c59f5
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-04 14:33:44 +0100

    add new component

---
+++ Frontend/src/app/shared/layout/base-view/base-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-view',
+  imports: [],
+  templateUrl: './base-view.component.html',
+  styleUrl: './base-view.component.scss'
+})
+export class BaseViewComponent {
+
+}
