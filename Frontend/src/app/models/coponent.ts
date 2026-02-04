344da530bbf36c6a8aa2ccf7fd142e015a282928
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-04 21:03:18 +0100

    create entities

---
+++ Frontend/src/app/models/coponent.ts
@@ -0,0 +1,6 @@
+export type Component = {
+    id: number,
+    code: string
+    name: string
+    description: string
+}
---
+++ Frontend/src/app/models/order.ts
@@ -0,0 +1,8 @@
+export type Order = {
+    id: number,
+    code: string,
+    OrderNumber: string,
+    Name: string,
+    Description: string,
+    Date: Date
+}
