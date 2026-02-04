d290477a40235255c537dc4852677a290716307a
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-04 22:46:14 +0100

    implement component service

---
+++ Frontend/src/app/services/component.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Component } from "../models/component";
+import { environment } from "../environments/environment.development";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ComponentService {
+
+    private app: string;
+    private api: string;
+
+    constructor(private http: HttpClient) {
+        this.app = environment.baseUrl;
+        this.api = "/component";
+    }
+
+    getComponents(): Observable<Component[]> {
+        return this.http.get<Component[]>(`${this.app}${this.api}`);
+    }
+
+    getComponent(id:number): Observable<Component> {
+        return this.http.get<Component>(`${this.app}${this.api}/${id}`)
+    }
+
+    createComponent(component: Component): Observable<Component>{
+        return this.http.post<Component>(`${this.app}${this.api}`, component);
+    }
+
+    //backend reuturn object with boolean
+    deleteComponent(id: number): Observable<void>{
+        return this.http.delete<void>(`${this.app}${this.api}/${id}`);
+    }
+}
