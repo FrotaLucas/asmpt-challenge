d9bdf9153faee161338d91f9e30cf27b873fe69e
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-04 16:00:32 +0100

    implement base-view

--- Frontend/src/app/shared/layout/base-view/base-view.component.ts
+++ Frontend/src/app/shared/layout/base-view/base-view.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
+// import { RouterOutlet } from "../../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-base-view',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, RouterOutlet],
   templateUrl: './base-view.component.html',
   styleUrl: './base-view.component.scss'
 })
