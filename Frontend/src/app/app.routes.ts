1be4d34955089f2366ec542619c6671f61404d17
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-04 14:57:03 +0100

    add route to new components

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { BaseViewComponent } from './shared/layout/base-view/base-view.component';
+import { ComponentComponent } from './shared/views/component/component.component';
+import { BoardComponent } from './shared/views/board/board.component';
 
 export const routes: Routes = [
     {
         path:' ', 
         component: BaseViewComponent,
+        children: [
+            {path: 'components', component: ComponentComponent},
+            {path: 'boards', component: BoardComponent}]
     }
 ];
