e97b0344eeebe623fa5d540248ceecb8cca1aea8
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-04 15:05:57 +0100

    move files

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { BaseViewComponent } from './shared/layout/base-view/base-view.component';
-import { ComponentComponent } from './shared/views/component/component.component';
-import { BoardComponent } from './shared/views/board/board.component';
+import { ComponentComponent } from './views/component/component.component';
+import { BoardComponent } from './views/board/board.component';
 
 export const routes: Routes = [
     {
