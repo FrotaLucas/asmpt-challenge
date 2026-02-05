b444c6a66fb859b0e70d4985033b590fb51c2a6a
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-05 17:01:15 +0100

    create new components

---
+++ Frontend/src/app/shared/components/add-component/add-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-component',
+  imports: [],
+  templateUrl: './add-component.component.html',
+  styleUrl: './add-component.component.scss'
+})
+export class AddComponentComponent {
+
+}
