497ac5d36866b058118dfecb22fceae1bc366fae
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-05 16:57:28 +0100

    rename folders

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { BaseViewComponent } from './shared/layout/base-view/base-view.component';
-import { ComponentComponent } from './views/component/component.component';
-import { BoardComponent } from './views/board/board.component';
-import { OrderComponent } from './views/order/order.component';
+import { ComponentComponent } from './views/component-table/component.component';
+import { BoardComponent } from './views/board-table/board.component';
+import { OrderComponent } from './views/order-table/order.component';
 
 export const routes: Routes = [
     {
