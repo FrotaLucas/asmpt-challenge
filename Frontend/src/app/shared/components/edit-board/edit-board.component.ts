2d2282654fb0c77fab12ed29747f8f10bc96df6f
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-06 12:36:42 +0100

    create edit board component

---
+++ Frontend/src/app/shared/components/edit-board/edit-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-board',
+  imports: [],
+  templateUrl: './edit-board.component.html',
+  styleUrl: './edit-board.component.scss'
+})
+export class EditBoardComponent {
+
+}
