54723b6df7120f40f2a2a98b5f40ca65d80eee2b
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-06 18:13:18 +0100

    use order board

--- Frontend/src/app/views/create-order/create-order.component.ts
+++ Frontend/src/app/views/create-order/create-order.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 import { MatLabel, MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 
+import { OrderBoardComponent } from '../../shared/components/order-board/order-board.component';
+
 @Component({
   selector: 'app-create-order',
-  imports: [MatFormField, MatLabel, MatIcon, MatInput, MatIconModule],
+  imports: [CommonModule, MatFormField, MatLabel, MatIcon, MatInput, MatIconModule, OrderBoardComponent],
   templateUrl: './create-order.component.html',
   styleUrl: './create-order.component.scss'
 })
+
 export class CreateOrderComponent {
+
+  boards: number[] = [0];
 
   constructor(){
 
@@ -19,7 +25,8 @@ export class CreateOrderComponent {
 
   addNewBoadComponent(): void{
 
+    this.boards.push(this.boards.length);
   }
 
-  
+
 }
