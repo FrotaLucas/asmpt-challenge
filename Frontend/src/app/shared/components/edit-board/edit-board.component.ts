3e33338144c674d5446d8299b1146a1f9ae284d3
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-06 14:14:17 +0100

    implement edit

--- Frontend/src/app/shared/components/edit-board/edit-board.component.ts
+++ Frontend/src/app/shared/components/edit-board/edit-board.component.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BoardService } from '../../../services/board.service';
+import { BoardDto } from '../../../models/board';
+
 
 @Component({
   selector: 'app-edit-board',
-  imports: [],
+  imports: [CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatIcon],
   templateUrl: './edit-board.component.html',
   styleUrl: './edit-board.component.scss'
 })
+
 export class EditBoardComponent {
 
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder, private _boardService: BoardService, private dialog: MatDialogRef<EditBoardComponent>,
+    private snackBar: MatSnackBar, @Inject(MAT_DIALOG_DATA) private board: BoardDto) {
+}
+
+ngOnInit(): void {
+  this.form = this.fb.group({
+    id: [this.board.id],
+    code: [this.board.code, Validators.required],
+    name: [this.board.name, Validators.required],
+    description: [this.board.description],
+    width: [this.board.width, Validators.required],
+    length: [this.board.length, Validators.required]
+  })
+}
+
+onSubmit(): void {
+  const board = this.form.value;
+
+  if(this.form.valid){
+  this._boardService.updateBoard(board).subscribe({
+    next: () => {
+      this.form.reset;
+      this.dialog.close(true);
+      this.snackBar.open("board updated successfully", "", { duration: 2000 });
+    },
+
+    error: (err) => console.error("error on updating board", err)
+  })
+}
+  }
 }
