f7acccfb92c0405d7d5e8d05cca4083faf451260
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-06 22:16:54 +0100

    explaining events

--- Frontend/src/app/shared/components/board-component/board-component.component.ts
+++ Frontend/src/app/shared/components/board-component/board-component.component.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatFormField, MatLabel, } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ComponentService } from '../../../services/component.service';
 import { MatSelectChange, MatSelectModule } from '@angular/material/select';
-import { ComponentDto } from '../../../models/component';
 
 @Component({
   selector: 'app-board-component',
@@ -13,35 +12,31 @@ import { ComponentDto } from '../../../models/component';
   styleUrl: './board-component.component.scss'
 })
 
-export class BoardComponentComponent implements OnInit {
+export class BoardComponentComponent {
 
   @Input() componentIndex!: number;
 
-  components!: ComponentDto[];
-  selectedComponent!: ComponentDto;
+  components: string[] = ['component1', 'component2', 'component3'];
+  selectedComponent: string = '';
 
   constructor(private _componentService: ComponentService) {
 
   }
 
-  ngOnInit(): void {
-    this.loadSelection();
-  }
 
   loadSelection(): void {
-    this._componentService.getComponents().subscribe({
-      next: (data) => {
-        this.components = data;
-      },
-    })
+
   }
 
   //1
-  onSelectionChange(component: ComponentDto): void {
-    this.selectedComponent = component;
-    console.log("content of event", component);
+  // onSelectionChange(value: any): void {
+  //   console.log("content of event", value);
+  // }
+
+  //2
+  onSelectionChange(event: MatSelectChange): void {
+    this.selectedComponent = event.value;
+    console.log("content of event", this.selectedComponent);
   }
-
-
 
 }
