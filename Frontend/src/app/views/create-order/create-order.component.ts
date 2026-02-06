4e4d577b9e3c0d2cd21ce6de9b2b15802834003a
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-06 16:59:51 +0100

    add basic functions

--- Frontend/src/app/views/create-order/create-order.component.ts
+++ Frontend/src/app/views/create-order/create-order.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+
+import { MatLabel, MatFormField } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+
 @Component({
   selector: 'app-create-order',
-  imports: [],
+  imports: [MatFormField, MatLabel, MatIcon, MatInput, MatIconModule],
   templateUrl: './create-order.component.html',
   styleUrl: './create-order.component.scss'
 })
 export class CreateOrderComponent {
 
+  constructor(){
+
+  }
+
+  addNewBoadComponent(): void{
+
+  }
+
+  
 }
