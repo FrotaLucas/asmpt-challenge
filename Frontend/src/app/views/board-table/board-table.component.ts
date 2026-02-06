547748db83dfa898bfd12dafbb360fe9d6bd640e
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-06 11:45:07 +0100

    implement first event  of board

--- Frontend/src/app/views/board-table/board-table.component.ts
+++ Frontend/src/app/views/board-table/board-table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../../services/board.service';
+import { BoardDto } from '../../models/board';
 
 @Component({
   selector: 'app-board-table',
@@ -6,6 +8,36 @@ import { Component } from '@angular/core';
   templateUrl: './board-table.component.html',
   styleUrl: './board-table.component.scss'
 })
-export class BoardTableComponent {
 
+
+export class BoardTableComponent implements OnInit {
+
+  listOfBoards!: BoardDto[];
+
+  constructor(private _boardService: BoardService){
+
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  refreshPage(): void {
+    this._boardService.getBoards().subscribe({
+      next: (data) => {
+        this.listOfBoards = data,
+      console.log(this.listOfBoards)},
+      
+
+      error: (err) => console.error("error on refreshing page", err)
+    })
+  }
+
+  deleteBoard(id: Number){
+
+  }
+
+  updateBoard(board: BoardDto){
+
+  }
 }
