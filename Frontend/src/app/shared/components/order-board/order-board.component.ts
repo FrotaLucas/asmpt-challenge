24be455b2e180a11aa30a90e161e7d6c856dd75c
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-07 13:25:47 +0100

    implement api to fetch boards

--- Frontend/src/app/shared/components/order-board/order-board.component.ts
+++ Frontend/src/app/shared/components/order-board/order-board.component.ts
@@ -1,29 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
-import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 import { MatLabel, MatFormField } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { BoardComponentComponent } from '../board-component/board-component.component';
 import { CommonModule } from '@angular/common';
+import { MatSelect, MatOption, MatSelectModule } from "@angular/material/select";
+import { BoardDto } from '../../../models/board';
 import { BoardService } from '../../../services/board.service';
 
 
 @Component({
   selector: 'app-order-board',
-  imports: [CommonModule, MatFormField, MatLabel, MatInput, MatIconModule, BoardComponentComponent],
+  imports: [CommonModule, MatFormField, MatSelectModule, MatLabel, MatIconModule, BoardComponentComponent, MatSelect, MatOption],
   templateUrl: './order-board.component.html',
   styleUrl: './order-board.component.scss'
 })
 
-export class OrderBoardComponent {
+export class OrderBoardComponent implements OnInit {
 
   components: number[] = [0];
 
+  boards!: BoardDto[];
+  selectedBoard!: BoardDto;
+
   @Input() boardIndex!: number;
+
+  constructor(private _serviceBoard: BoardService){
+
+  }
+
+  ngOnInit(): void {
+      this.loadBoards();
+  }
 
   addNewComponent() : void{
 
     this.components.push(this.components.length);
+  }
+
+  loadBoards(): void {
+    this._serviceBoard.getBoards().subscribe({
+      next: (data) => {
+        this.boards = data;
+      }
+    })
+  }
+
+  onSelectionBoard(board: BoardDto){
+    this.selectedBoard = board;
   }
 }
