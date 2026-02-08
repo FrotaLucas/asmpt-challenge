81691a1523a7133cd24df16d6ead0e15ffc60925
FrotaLucas <lucasdiasfrota@gmail.com>
2026-02-08 15:40:36 +0100

    fix

--- Backend/Application/Dtos/Order/OrderRequestDto.cs
+++ Backend/Application/Dtos/Order/OrderRequestDto.cs
@@ -6,13 +6,13 @@ namespace Backend.Application.DTOs.Order
     //POST somente. Nao tem PUT
     public class OrderRequestDto
     {
-        public String OrderNumber { get; set; } = string.Empty;
+        public string OrderNumber { get; set; } = string.Empty;
 
         public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
 
-        public DateTime OrderDate { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.Now;
         //1
         public List<OrderBoardRequestDto> OrderBoards { get; set; } = new List<OrderBoardRequestDto>();
     }
